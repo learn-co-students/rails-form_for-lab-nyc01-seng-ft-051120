29f5dd206827f16bb74f64c69d73c622fed86f82
kevinneyer <kevinneyer@gmail.com>
2020-06-06 13:33:53 -0400

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,37 @@
+class SchoolClassesController < ApplicationController
+    
+    def index
+        @school_classes = SchoolClass.all 
+      end
+    
+      def show
+        @school_class= SchoolClass.find(params[:id])
+      end
+    
+      def new
+        @school_class = SchoolClass.new
+      end
+    
+      def create
+        @school_class = SchoolClass.create(class_params)
+
+        redirect_to school_class_path(@school_class.id)
+      end
+    
+      def edit
+        @school_class = SchoolClass.find(params[:id])
+      end
+    
+      def update
+        @school_class = SchoolClass.find(params[:id]) 
+        @school_class.update(class_params)
+
+        redirect_to school_class_path(@school_class.id)
+      end
+    
+      private
+    
+      def class_params
+        params.require(:school_class).permit(:title, :room_number)
+      end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Jim", last_name: "Neyer")
+Student.create(first_name: "Dorothy", last_name: "Neyer")
+Student.create(first_name: "Kelly", last_name: "Eves")
+Student.create(first_name: "Kevin", last_name: "Neyer")
+Student.create(first_name: "Brian", last_name: "Neyer")
+Student.create(first_name: "G", last_name: "Morrison")
+Student.create(first_name: "Tess", last_name: "Colwell")
+
+SchoolClass.create(title: "Science", room_number: 1)
+SchoolClass.create(title: "Math", room_number: 9)
+SchoolClass.create(title: "Band", room_number: 22)
+SchoolClass.create(title: "Enlgish", room_number: 4)
+SchoolClass.create(title: "Biology", room_number: 18)
